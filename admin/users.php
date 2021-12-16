cbe10375f4a54c4253d2c8c57adc33f5fff3596d
JDawwgy <jordan5057@gmail.com>
2021-12-15 17:22:51 -0800

    added create new user function

--- admin/users.php
+++ admin/users.php
@@ -37,8 +37,9 @@ $users = User::find_all();
             <div class="col-lg-12">
                 <h1 class="page-header">
                     Users
-                    <small>Subheading</small>
                 </h1>
+
+                <a href="add_user.php" class="btn btn-primary">Add User</a>
 
                 <div class="col-md-12">
 
